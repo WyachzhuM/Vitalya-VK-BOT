4c12f478f0882fb47b846c4a9a2c6c4e72a08d01
LPLP <lilpophill@gmail.com>
2025-02-27 18:08:36 +0300

    soma changes

--- Core/Vk.cs
+++ Core/Vk.cs
@@ -2,8 +2,10 @@ using VkNet.Model;
 
 namespace vkbot_vitalya.Core;
 
-public static class Vk {
-    public static string PingUser(this User user, string? name = null) {
+public static class Vk 
+{
+    public static string PingUser(this User user, string? name = null) 
+    {
         return $"[id{user.Id}|{name ?? user.FirstName + " " + user.LastName}]";
     }
 }
